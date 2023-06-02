e6f63500b7d55905b47048c59743b7c1d074003f
Abhishek Patil <abhishekpatil56381@gmail.com>
2023-06-02 19:09:16 +0530

    api integration

--- music.js
+++ music.js
@@ -38,21 +38,20 @@ function togglePlayPause() {
    }
 }
 
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  fetchMusic();
-});
+
+
+// **************** API for searched music **********************
 
 async function fetchMusic() {
   const searchTerm = document.getElementById('search-input').value;
-  const apiKey = 'f04e45b808fb2402087ccdefc7a6ea6f'; // Replace with your Last.fm API key
+  const apiKey = '	54eea564740c511910bb580592d841f7'; // Replace with your Last.fm API key
   const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${encodeURIComponent(searchTerm)}&api_key=${apiKey}&format=json`;
 
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
     
-    console.log(data);
+    //console.log(data);
     const track = data.results?.trackmatches?.track[0];
     if (track) {
       const songName = track.name;
@@ -77,5 +76,154 @@ async function fetchMusic() {
     document.getElementById('audio').src = '';
   }
 }
+
+// *************API for getting top song ****************** 
+
+async function fetchTopSong() {
+  const apiKey = '54eea564740c511910bb580592d841f7'; // Replace with your Last.fm API key
+  const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.error) {
+      console.error('Error fetching top song:', data.message);
+      return;
+    }
+    //console.log(data);
+    const topTrack = data.tracks.track[0];
+    const songName = topTrack.name;
+    const artistName = topTrack.artist.name;
+    const playCount = topTrack.playcount;
+
+
+    document.getElementById('trending-song').innerHTML = songName;
+    document.getElementById('trending-hit-artist').innerHTML = artistName;
+    document.getElementById('trending-hit-listeners').innerHTML = playCount;
+  } catch (error) {
+    console.error('Error fetching top song:', error);
+  }
+}
+
+fetchTopSong();
+
+
+// *************** Playing trending song ******************
+
+let playTrendingSong = async () => {
+  try {
+    const apiKey = '54eea564740c511910bb580592d841f7'; // Replace with your Last.fm API key
+    const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.error) {
+      console.error('Error fetching top song:', data.message);
+      return;
+    }
+
+    const topTrack = data.tracks.track[0];
+    const songName = topTrack.name;
+    const artistName = topTrack.artist.name;
+
+    document.getElementById('playing-song').innerHTML = songName;
+    document.getElementById('playing-song-artist').innerHTML = artistName;
+  } catch (error) {
+    console.error('Error fetching top song:', error);
+  }
+};
+
+document.getElementById('trending-song').addEventListener('click', playTrendingSong);
+
+
+// ***************** API for getting top 5 tracks **********************
+
+async function fetchTop5Song() {
+  const apiKey = '54eea564740c511910bb580592d841f7'; // Replace with your Last.fm API key
+  const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.error) {
+      console.error('Error fetching top song:', data.message);
+      return;
+    }
+    //console.log(data);
+
+    for (let i = 1; i<6; i++)
+    {
+      const topTrack = data.tracks.track[i];
+      const songName = topTrack.name;
+      const artistName = topTrack.artist.name;
+
+      document.getElementById(`song${i}`).innerHTML = songName;
+      document.getElementById(`artist${i}`).innerHTML = artistName;
+
+    }
+  } catch (error) {
+    console.error('Error fetching top song:', error);
+  }
+}
+
+fetchTop5Song();
+
+
+// ************** Play-pause implementation ***************
+
+
+var isPlaying = false;
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'Vaaste - Dhvani Bhanushali-(PagalWorld.Ink).mp3');
+
+    function togglePlayPause() {
+        var pauseIcon = document.querySelector('#pause i');
+
+        if (isPlaying) {
+            audioElement.pause();
+            isPlaying = false;
+            pauseIcon.classList.remove('bx-pause-circle');
+            pauseIcon.classList.add('bx-play-circle');
+        } else {
+            audioElement.play();
+            isPlaying = true;
+            pauseIcon.classList.remove('bx-play-circle');
+            pauseIcon.classList.add('bx-pause-circle');
+        }
+    }
+
+    var pauseButton = document.querySelector('#pause');
+    pauseButton.addEventListener('click', togglePlayPause);
+
+
+// *********** song duration ****************
+
+let Song = document.getElementById('audio');
+  let progress = document.getElementById('progress');
+
+  Song.addEventListener('loadedmetadata', function () {
+    progress.max = Song.duration;
+    progress.value = Song.currentTime;
+  });
+
+  Song.addEventListener('timeupdate', function () {
+    progress.value = Song.currentTime;
+  });
+
+  progress.oninput = function () {
+    Song.currentTime = progress.value;
+  };
+
+  progress.onchange = function () {
+    Song.play();
+  };
+
+
+
+
+
 
 
